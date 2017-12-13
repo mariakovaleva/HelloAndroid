b84086614799287157647c809fa0af08fba9e0bf
Mariia Kovaleva <mariakovaleva@outlook.com>
2017-12-13 17:00:03 +0100

    done with the top View

--- app/libs/CircleImageView-master/sample/src/main/java/de/hdodenhof/circleimageview/sample/MainActivity.java
+++ app/libs/CircleImageView-master/sample/src/main/java/de/hdodenhof/circleimageview/sample/MainActivity.java
@@ -1,9 +1,9 @@
-package com.mariako.android.helloandroid;
+package de.hdodenhof.circleimageview.sample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
